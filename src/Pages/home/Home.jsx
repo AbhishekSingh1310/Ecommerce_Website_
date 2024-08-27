627fd147dc9514b7e8846b716259e96abc2255f7
Abhishek Singh <147223566+AbhishekSingh1310@users.noreply.github.com>
2024-08-27 17:42:46 +0530

    Update Home.jsx

--- src/Pages/home/Home.jsx
+++ src/Pages/home/Home.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react'
-import Layout from '../../Components/Layout/layout'
+import Layout from '../../Components/Layout/Layout'
 import myContext from '../../Context/data/myContext'
 import Herosection from '../../Components/Herosection/Herosection'
 import Filter from '../../Components/Filter/Filter'
